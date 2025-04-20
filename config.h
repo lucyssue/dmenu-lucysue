510f253c546c5e0f06a9b9bf2e75769ab1a60058
lucy <lucysue.comfy001@passmail.net>
2025-04-20 17:42:36 +0200

    Adding dmenu source code

---
+++ config.h
@@ -0,0 +1,21 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+#include "/home/uz3r/.cache/wal/colors-wal-dmenu.h"
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int fuzzy = 1;
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"VictorMono Nerd Font Mono:size=13"
+};
+static const int user_bh = 5;
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 0;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
