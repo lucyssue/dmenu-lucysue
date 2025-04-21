a2217cb3b4300beefa8f66a278373a174e4596f3
lucy <lucyssue@pm.me>
2025-04-21 13:16:17 +0200

    Adding bash script for better pywal handling

--- config.h
+++ config.h
@@ -1,8 +1,6 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-#include "/home/uz3r/.cache/wal/colors-wal-dmenu.h"
-
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 static int fuzzy = 1;
 /* -fn option overrides fonts[0]; default X11 font or font set */
@@ -12,6 +10,12 @@ static const char *fonts[] = {
 static const int user_bh = 5;
 static const char *prompt      = "Search...";      /* -p  option; prompt to the left of input field */
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#bbbbbb", "#222222" },
+	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeOut] = { "#000000", "#00ffff" },
+};
 static unsigned int lines      = 0;
 
 /*
